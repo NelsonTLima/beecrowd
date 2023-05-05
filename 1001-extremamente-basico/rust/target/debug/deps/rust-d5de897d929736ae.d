40387c7a945120cc3fd7312fbc24371eec5aea90
NelsonTLima <nelsontlima@gmail.com>
2023-05-05 15:02:33 -0300

    removed some rust files.

--- 1001-extremamente-basico/rust/target/debug/deps/rust-d5de897d929736ae.d
+++
@@ -1,5 +0,0 @@
-/Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/target/debug/deps/rust-d5de897d929736ae: src/main.rs
-
-/Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/target/debug/deps/rust-d5de897d929736ae.d: src/main.rs
-
-src/main.rs:
--- 1001-extremamente-basico/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/target/debug/rust: /Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/src/main.rs
