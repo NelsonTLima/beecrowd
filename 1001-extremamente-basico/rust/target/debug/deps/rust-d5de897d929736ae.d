0473c062042d01127f2d7ab6c6c19c0dc95324d7
NelsonTLima <nelsontlima@gmail.com>
2023-05-04 21:03:27 -0300

    added rust.

---
+++ 1001-extremamente-basico/rust/target/debug/deps/rust-d5de897d929736ae.d
@@ -0,0 +1,5 @@
+/Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/target/debug/deps/rust-d5de897d929736ae: src/main.rs
+
+/Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/target/debug/deps/rust-d5de897d929736ae.d: src/main.rs
+
+src/main.rs:
---
+++ 1001-extremamente-basico/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/target/debug/rust: /Users/nelsonlima/projects/beecrowd/1001-extremamente-basico/rust/src/main.rs
