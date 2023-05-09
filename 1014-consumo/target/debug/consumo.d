65399ef37f797a658bc9af02c735af0a1055c542
NelsonTLima <nelsontlima@gmail.com>
2023-05-08 22:25:32 -0300

    solved in rust.

---
+++ 1014-consumo/target/debug/consumo.d
@@ -0,0 +1,1 @@
+/Users/nelsonlima/projects/beecrowd-exercises/1014-consumo/target/debug/consumo: /Users/nelsonlima/projects/beecrowd-exercises/1014-consumo/src/main.rs
---
+++ 1014-consumo/target/debug/deps/consumo-fe817b9e44010305.d
@@ -0,0 +1,5 @@
+/Users/nelsonlima/projects/beecrowd-exercises/1014-consumo/target/debug/deps/consumo-fe817b9e44010305: src/main.rs
+
+/Users/nelsonlima/projects/beecrowd-exercises/1014-consumo/target/debug/deps/consumo-fe817b9e44010305.d: src/main.rs
+
+src/main.rs:
