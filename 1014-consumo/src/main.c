f6ce4635a02c360e67d1ac2e4510b730a65d6e8c
NelsonTLima <nelsontlima@gmail.com>
2023-05-11 18:52:16 -0300

    spaces between sections.

--- 1014-consumo/src/main.c
+++ 1014-consumo/src/main.c
@@ -2,7 +2,9 @@
 
 int main() {
   double distancia_percorrida, litros_gastos, consumo;
+
   scanf("%lf\n%lf", &distancia_percorrida, &litros_gastos);
   consumo = distancia_percorrida/litros_gastos;
+
   printf("%.3lf km/l\n", consumo);
 }
